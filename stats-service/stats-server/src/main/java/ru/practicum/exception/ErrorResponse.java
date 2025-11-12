97c3c7645ba249484e8d00a5d3f102b5a4134c8b
marina <shadrinam2012@yandex.ru>
2025-11-12 09:13:26 +0300

    feat - diploma, 1 stage

--- stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorResponse.java
+++ stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorResponse.java
@@ -1,7 +1,6 @@
 package ru.practicum.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import ru.practicum.dto.HitDto;
